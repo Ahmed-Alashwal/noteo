ed50a1267a7b16f3238cc42cc7a8224109aa09a4
IT-Developer2025 <yemenideveloper2025@gmail.com>
2026-03-25 22:01:14 +0300

    1- Finish UI 2- Register NOTE model adapter and open NOTE box.

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,3 +1,3 @@
 abstract class AppString{
-  
+  static const String kNoteBox = "noteBox";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/core/constants/strings.dart';
+import 'package:note_app/features/note/data/models/note_model.dart';
 import 'package:note_app/features/splash/presentation/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  Hive.initFlutter();
+  Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox(AppString.kNoteBox);
+  
   runApp(const Noteo());
 }
 
