4890bcc4cdc25cbe44a716a176c5161a737312fa
IT-Developer2025 <yemenideveloper2025@gmail.com>
2026-03-27 14:46:55 +0300

    - Implement DELETE NOTE cubit.

--- lib/features/note/presentation/manager/delete_note_cubit/delete_note_cubit.dart
+++ lib/features/note/presentation/manager/delete_note_cubit/delete_note_cubit.dart
@@ -1,8 +1,23 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/features/note/domain/usecases/delete_note_use_case.dart';
 
 part 'delete_note_state.dart';
 
 class DeleteNoteCubit extends Cubit<DeleteNoteState> {
-  DeleteNoteCubit() : super(DeleteNoteInitial());
+  DeleteNoteCubit({required this.deleteNoteUseCase})
+    : super(DeleteNoteInitial());
+
+  final DeleteNoteUseCase deleteNoteUseCase;
+
+  Future<void> deleteNote({required String noteId}) async {
+    emit(DeleteNoteLoading());
+
+    var result = await deleteNoteUseCase.call(noteId);
+
+    result.fold(
+      (failure) =>
+          emit(DeleteNoteFailure(errMessage: "Error while delete a note!")),
+      (success) => emit(DeleteNoteSuccess()),
+    );
+  }
 }
