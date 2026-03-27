49d855f0f3b83514219e5728724efdab55ab5e53
IT-Developer2025 <yemenideveloper2025@gmail.com>
2026-03-27 14:41:03 +0300

    - Implement CREATE NOTE cubit.

--- lib/features/note/presentation/manager/create_note_cubit/create_note_cubit.dart
+++ lib/features/note/presentation/manager/create_note_cubit/create_note_cubit.dart
@@ -1,8 +1,24 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/features/note/domain/entities/note_entity.dart';
+import 'package:note_app/features/note/domain/usecases/create_note_use_case.dart';
 
 part 'create_note_state.dart';
 
 class CreateNoteCubit extends Cubit<CreateNoteState> {
-  CreateNoteCubit() : super(CreateNoteInitial());
+  CreateNoteCubit({required this.createNoteUseCase})
+    : super(CreateNoteInitial());
+
+  final CreateNoteUseCase createNoteUseCase;
+
+  Future<void> createNote({required NoteEntity noteEntity}) async {
+    emit(CreateNoteLoading());
+
+    var result = await createNoteUseCase.call(noteEntity);
+    result.fold(
+      (failure) => emit(
+        CreateNoteFailure(errMessage: "Error while create a new note!"),
+      ),
+      (success) => emit(CreateNoteSuccess()),
+    );
+  }
 }
