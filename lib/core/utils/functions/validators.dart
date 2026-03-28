fedcdec58ce3ba2ed6ee86d3c450d9fafb6eaa41
IT-Developer2025 <yemenideveloper2025@gmail.com>
2026-03-28 20:19:57 +0300

    - Use CREATE NOTE cubit in the project.

--- lib/core/utils/functions/validators.dart
+++ lib/core/utils/functions/validators.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/core/utils/functions/show_snack_bar.dart';
+import 'package:note_app/features/note/domain/entities/note_entity.dart';
+import 'package:note_app/features/note/presentation/manager/create_note_cubit/create_note_cubit.dart';
+import 'package:note_app/features/note/presentation/manager/fetch_all_notes_cubit/fetch_all_notes_cubit.dart';
 
 void validateWholeNoteForm(
   BuildContext context, {
@@ -9,6 +13,13 @@ void validateWholeNoteForm(
   required String message,
 }) {
   if (formKey.currentState!.validate()) {
+    context.read<CreateNoteCubit>().createNote(
+      noteEntity: NoteEntity(
+        title: titleController.text,
+        content: contentController.text,
+      ),
+    );
+    context.read<FetchAllNotesCubit>().fetchAllNotes();
     showSucessSnackBar(context, message: "Note $message successfully.");
     titleController.clear();
     contentController.clear();
--- lib/features/note/presentation/widgets/create_note_form.dart
+++ lib/features/note/presentation/widgets/create_note_form.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
+import 'package:note_app/core/utils/functions/service_locator.dart';
 import 'package:note_app/core/utils/functions/validators.dart';
 import 'package:note_app/core/widgets/custom_button.dart';
 import 'package:note_app/core/widgets/custom_text_form_field.dart';
+import 'package:note_app/features/note/data/repository/note_repository_impl.dart';
+import 'package:note_app/features/note/domain/usecases/create_note_use_case.dart';
+import 'package:note_app/features/note/presentation/manager/create_note_cubit/create_note_cubit.dart';
 
 class CreateNoteForm extends StatelessWidget {
   const CreateNoteForm({
@@ -18,43 +23,54 @@ class CreateNoteForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          CustomTextFormField(
-            autofocus: true,
-            labelText: "Title",
-            hintText: "Title...",
-            controller: titleController,
-            validator: (value) {
-              return noteTFFValidator(data: value);
-            },
-          ),
-          const Gap(12),
-          CustomTextFormField(
-            maxLines: 5,
-            labelText: "Content",
-            hintText: "Content...",
-            controller: contentController,
-            validator: (value) => noteTFFValidator(data: value),
-          ),
-          const Gap(24),
-          CustomButton(
-            label: "Save",
-            hasShadow: true,
-            onTap: () {
-              validateWholeNoteForm(
-                context,
-                formKey: _formKey,
-                titleController: titleController,
-                contentController: contentController,
-                message: "Created",
-              );
-            },
-          ),
-        ],
+    return BlocProvider(
+      create: (context) => CreateNoteCubit(
+        createNoteUseCase: CreateNoteUseCase(
+          noteRepository: di.get<NoteRepositoryImpl>(),
+        ),
+      ),
+      child: Builder(
+        builder: (context) {
+          return Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                CustomTextFormField(
+                  autofocus: true,
+                  labelText: "Title",
+                  hintText: "Title...",
+                  controller: titleController,
+                  validator: (value) {
+                    return noteTFFValidator(data: value);
+                  },
+                ),
+                const Gap(12),
+                CustomTextFormField(
+                  maxLines: 5,
+                  labelText: "Content",
+                  hintText: "Content...",
+                  controller: contentController,
+                  validator: (value) => noteTFFValidator(data: value),
+                ),
+                const Gap(24),
+                CustomButton(
+                  label: "Save",
+                  hasShadow: true,
+                  onTap: () {
+                    validateWholeNoteForm(
+                      context,
+                      formKey: _formKey,
+                      titleController: titleController,
+                      contentController: contentController,
+                      message: "Created",
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
