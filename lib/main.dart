5978d4cf3bc2ec123e0064d52e9557dd26950de1
IT-Developer2025 <yemenideveloper2025@gmail.com>
2026-03-28 16:12:51 +0300

    - Provide FETCH NOTE cubit to all project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/core/constants/strings.dart';
 import 'package:note_app/core/utils/functions/service_locator.dart';
+import 'package:note_app/features/note/data/repository/note_repository_impl.dart';
 import 'package:note_app/features/note/domain/entities/note_entity.dart';
+import 'package:note_app/features/note/domain/usecases/fetch_all_notes_use_case.dart';
+import 'package:note_app/features/note/presentation/manager/fetch_all_notes_cubit/fetch_all_notes_cubit.dart';
 import 'package:note_app/features/splash/presentation/screens/splash_screen.dart';
 
 void main() async {
@@ -12,8 +16,17 @@ void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(NoteEntityAdapter());
   await Hive.openBox<NoteEntity>(AppString.kNoteBox);
-  
-  runApp(const Noteo());
+
+  runApp(
+    BlocProvider(
+      create: (context) => FetchAllNotesCubit(
+        fetchAllNotesUseCase: FetchAllNotesUseCase(
+          noteRepository: di.get<NoteRepositoryImpl>(),
+        ),
+      )..fetchAllNotes(),
+      child: const Noteo(),
+    ),
+  );
 }
 
 class Noteo extends StatelessWidget {
