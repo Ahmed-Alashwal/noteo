e1746ffbc0a3e2a6047ff3e2f4eed3991dfb6d35
Ahmed-Alashwal <>
2026-04-08 13:09:41 +0300

    feat(strings): centralize string resources in single constants file

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,3 +1,19 @@
-abstract class AppString{
+abstract class AppString {
+  static const String kDeveloperName = "Ahmed Alashwal";
+  static const String kAppName = "Noteo";
+  static const String kVersion = 'Version 1.0.0';
+  static const String kFontFamily = "Merriweather";
+  static const String kNoteoLogo = "assets/icons/shared/noteo_logo.svg";
+  static const String kHappyCircle = "assets/icons/shared/happy_circle.svg";
+  static const String kSadCircle = "assets/icons/shared/sad_circle.svg";
+  static const String kPen = "assets/icons/shared/pen.svg";
+  static const String kTopCircles = "assets/icons/shared/top_circles.svg";
+  static const String kBottomCircles = "assets/icons/shared/bottom_circles.svg";
+  static const String kLogin = "Login";
+  static const String kRegister = "Register";
+  static const String kLogout = "Logout";
+  static const String kAbout = "About";
+  static const String kProfile = "Profile";
   static const String kNoteBox = "noteBox";
+  static const String kCancel = "Cancel";
 }
