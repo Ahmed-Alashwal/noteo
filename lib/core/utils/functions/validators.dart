d50a91aeb11c4611ae0c730d412a12faa8bef9b6
Ahmed-Alashwal <>
2026-04-09 12:37:11 +0300

    feat(auth): create auth validators.

--- lib/core/utils/functions/validators.dart
+++ lib/core/utils/functions/validators.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/core/constants/strings.dart';
 import 'package:note_app/core/utils/functions/show_snack_bar.dart';
 import 'package:note_app/features/note/domain/entities/note_entity.dart';
 import 'package:note_app/features/note/presentation/manager/create_note_cubit/create_note_cubit.dart';
 import 'package:note_app/features/note/presentation/manager/fetch_all_notes_cubit/fetch_all_notes_cubit.dart';
 import 'package:note_app/features/note/presentation/manager/update_note_cubit/update_note_cubit.dart';
+
+String? validateUsername(String input) {
+  final bool isMatch = RegExp(r'^[A-Za-z_]{3,}').hasMatch(input.trim());
+  return isMatch
+      ? null
+      : 'Enter first and last name (letters only, min 3 chars each)';
+}
+
+String? validateEmail(String input) {
+  final bool isMatch = RegExp(
+    r'^[a-zA-Z0-9_]+@gmail.com$',
+  ).hasMatch(input.trim());
+  return isMatch
+      ? null
+      : 'Enter a valid email address (e.g., name@example.com)';
+}
+
+String? validatePassword(String input) {
+  final bool isMatch = RegExp(r'^[A-Za-z0-9]{6,}$').hasMatch(input.trim());
+  return isMatch ? null : 'Min 6 characters, letters and numbers only';
+}
+
+// TFF = TextFormField
+// this function will return NULL every time
+// becuase we accepte any text for note's
+// title and content.
+String? noteTFFValidator(String input) {
+  return null;
+}
 
 void validateWholeNoteForm(
   BuildContext context, {
@@ -15,7 +45,7 @@ void validateWholeNoteForm(
   required String message,
 }) {
   if (formKey.currentState!.validate()) {
-    if (message == "Created") {
+    if (message == AppString.kCreate) {
       context.read<CreateNoteCubit>().createNote(
         noteEntity: NoteEntity(
           id: uuid.v4(),
@@ -26,7 +56,7 @@ void validateWholeNoteForm(
         ),
       );
     }
-    if (message == "Updated") {
+    if (message == AppString.kUpdate) {
       context.read<UpdateNoteCubit>().updateNote(
         updatedNoteEntity: NoteEntity(
           id: noteId!,
@@ -48,11 +78,4 @@ void validateWholeNoteForm(
       message: "${message.substring(0, 6)} note error",
     );
   }
-}
-
-// TFF = TextFormField
-String? noteTFFValidator({String? data}) {
-  return data == null || data.isEmpty
-      ? "This field should not be empty!"
-      : null;
 }
