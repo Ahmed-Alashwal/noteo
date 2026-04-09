c339eb9410426f6b1bdd92da03556d287134ba00
Ahmed-Alashwal <>
2026-04-09 17:34:21 +0300

    feat: add flutter secure storage, auth repo impl to the service locator.

--- lib/core/utils/functions/service_locator.dart
+++ lib/core/utils/functions/service_locator.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
-import 'package:note_app/core/utils/functions/api_service.dart';
+import 'package:note_app/core/api/dio_consumer.dart';
+import 'package:note_app/features/auth/data/data_sources/remote/auth_remote_data_source.dart';
+import 'package:note_app/features/auth/data/repository/auth_repository_impl.dart';
 import 'package:note_app/features/note/data/data_sources/local/note_local_data_source.dart';
 import 'package:note_app/features/note/data/data_sources/remote/note_remote_data_source.dart';
 import 'package:note_app/features/note/data/repository/note_repository_impl.dart';
@@ -8,12 +11,24 @@ import 'package:note_app/features/note/data/repository/note_repository_impl.dart
 final GetIt di = GetIt.instance;
 
 Future<void> setupServiceLocator() async {
-  di.registerSingleton<ApiService>(ApiService(dio: Dio()));
+  // flutter secure storage
+  di.registerLazySingleton<FlutterSecureStorage>(
+    () => const FlutterSecureStorage(),
+  );
+  // Auth
+  di.registerLazySingleton<AuthRepositoryImpl>(
+    () => AuthRepositoryImpl(
+      authRemoteDataSource: AuthRemoteDataSourceImpl(
+        apiConsumer: DioConsumer(dio: Dio()),
+      ),
+    ),
+  );
+  // Notes
   di.registerSingleton<NoteRepositoryImpl>(
     NoteRepositoryImpl(
       noteLocalDataSource: NoteLocalDataSourceImpl(),
       noteRemoteDataSource: NoteRemoteDataSourceImpl(
-        apiService: di.get<ApiService>(),
+        apiConsumer: DioConsumer(dio: Dio()),
       ),
     ),
   );
