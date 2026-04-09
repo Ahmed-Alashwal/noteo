ad223243be716c26153cd6228d1b2942db78e33c
Ahmed-Alashwal <>
2026-04-09 18:15:59 +0300

    refactor(core): consolidate snackbar logic into single reusable base function

--- lib/core/utils/functions/show_snack_bar.dart
+++ lib/core/utils/functions/show_snack_bar.dart
@@ -2,32 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:note_app/core/constants/colors.dart';
 import 'package:note_app/core/constants/sizes.dart';
+import 'package:note_app/core/constants/strings.dart';
 import 'package:note_app/core/constants/text_style.dart';
 
-ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSucessSnackBar(
+ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSnackBar(
+  BuildContext context, {
+  required Color backgroundColor,
+  required String icon,
+  required String message,
+}) {
+  ScaffoldMessenger.of(context).clearSnackBars();
+  return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      padding: EdgeInsets.zero,
+      content: Padding(
+        padding: AppSizes.pH12,
+        child: ListTile(
+          contentPadding: EdgeInsets.zero,
+          leading: SvgPicture.asset(icon, width: 24, height: 24),
+          title: Text(message, style: AppTextStyles.small12Bold),
+        ),
+      ),
+      showCloseIcon: true,
+      backgroundColor: backgroundColor,
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(borderRadius: AppSizes.r12),
+    ),
+  );
+}
+
+ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showSuccessSnackBar(
   BuildContext context, {
   required String message,
 }) {
-  ScaffoldMessenger.of(context).clearSnackBars(); // sure about it?!
-  return ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Row(
-        spacing: 8,
-        children: [
-          SvgPicture.asset(
-            "assets/icons/happy_circle.svg",
-            width: 24,
-            height: 24,
-          ), //
-          Text(message, style: AppTextStyles.small14Bold),
-        ],
-      ),
-      showCloseIcon: true,
-      backgroundColor: AppColors.success,
-      behavior: SnackBarBehavior.floating,
-      shape: RoundedRectangleBorder(borderRadius: AppSizes.r12),
-      elevation: 0,
-    ),
+  return showSnackBar(
+    context,
+    backgroundColor: AppColors.success,
+    icon: AppString.kHappyCircle,
+    message: message,
   );
 }
 
@@ -35,25 +47,10 @@ ScaffoldFeatureController<SnackBar, SnackBarClosedReason> showErrorSnackBar(
   BuildContext context, {
   required String message,
 }) {
-  ScaffoldMessenger.of(context).clearSnackBars(); // sure about it?!
-  return ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Row(
-        spacing: 8,
-        children: [
-          SvgPicture.asset(
-            "assets/icons/sad_circle.svg",
-            width: 24,
-            height: 24,
-          ), //
-          Text(message, style: AppTextStyles.small14Bold),
-        ],
-      ),
-      showCloseIcon: true,
-      backgroundColor: AppColors.error,
-      behavior: SnackBarBehavior.floating,
-      shape: RoundedRectangleBorder(borderRadius: AppSizes.r12),
-      elevation: 0,
-    ),
+  return showSnackBar(
+    context,
+    backgroundColor: AppColors.error,
+    icon: AppString.kSadCircle,
+    message: message,
   );
 }
