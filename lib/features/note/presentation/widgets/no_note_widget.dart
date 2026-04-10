9a5e0c644495780b924bb673d784919d91a7cdd1
Ahmed-Alashwal <>
2026-04-10 10:45:53 +0300

    refactor(widget): replace hardcoded strings and and remove unncessary spaces.

--- lib/features/note/presentation/widgets/no_note_widget.dart
+++ lib/features/note/presentation/widgets/no_note_widget.dart
@@ -1,40 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
+import 'package:note_app/core/constants/strings.dart';
 import 'package:note_app/core/constants/text_style.dart';
 import 'package:note_app/features/note/presentation/screens/create_note_screen.dart';
 
 class NoNoteWidget extends StatelessWidget {
-  const NoNoteWidget({
-    super.key,
-  });
+  const NoNoteWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: GestureDetector(
-          onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const CreateNoteScreen(),
-            ),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SvgPicture.asset(
-                "assets/icons/pen.svg",
-                width: 40,
-                height: 40,
-              ),
-              const Gap(12),
-              const Text(
-                "No notes yet. Tap to add a note",
-                style: AppTextStyles.small14Regular,
-              ),
-            ],
-          ),
+      child: GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const CreateNoteScreen()),
         ),
-      );
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SvgPicture.asset(AppString.kPen, width: 40, height: 40),
+            const Gap(12),
+            const Text(
+              "No notes yet. Tap to add a note",
+              style: AppTextStyles.small12Regular,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
