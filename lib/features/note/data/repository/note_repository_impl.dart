273620e479642c2a0e1489506ca64d3475573688
Ahmed-Alashwal <>
2026-04-11 07:29:59 +0300

    feat(note): temporarily use remote note data source over local
    - Switched active data source to remote for testing/transition period
    - Local data source remains intact but is not in use

--- lib/features/note/data/repository/note_repository_impl.dart
+++ lib/features/note/data/repository/note_repository_impl.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:note_app/core/errors/failure.dart';
 import 'package:note_app/features/note/data/data_sources/local/note_local_data_source.dart';
 import 'package:note_app/features/note/data/data_sources/remote/note_remote_data_source.dart';
+import 'package:note_app/features/note/data/models/note_model.dart';
 import 'package:note_app/features/note/domain/entities/note_entity.dart';
 import 'package:note_app/features/note/domain/repository/note_repository.dart';
 
@@ -18,14 +19,17 @@ class NoteRepositoryImpl extends NoteRepository {
   @override
   Future<Either<Failure, List<NoteEntity>>> fetchAllNotes() async {
     try {
-      List<NoteEntity> localNotes = noteLocalDataSource.fetchAllNotes();
-      return right(localNotes);
-    } catch (e) {
-      if (e is DioException) {
-        return left(ServerFailure.fromDioError(e));
-      } else {
-        return left(ServerFailure(errMessage: e.toString()));
+      // List<NoteEntity> localNotes = noteLocalDataSource.fetchAllNotes();
+      Response response = await noteRemoteDataSource.fetchAllNotes();
+      List<NoteEntity> remoteNotes = [];
+      for (var note in response.data) {
+        remoteNotes.add(NoteModel.fromJson(note));
       }
+      return right(remoteNotes);
+    } on DioException catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
+    } catch (e) {
+      return left(ServerFailure(errMessage: "[LOCAL-DB]:Error reading notes!"));
     }
   }
 
@@ -34,15 +38,13 @@ class NoteRepositoryImpl extends NoteRepository {
     required NoteEntity noteEntity,
   }) async {
     try {
-      await noteLocalDataSource.createNote(noteEntity: noteEntity);
+      // await noteLocalDataSource.createNote(noteEntity: noteEntity);
       await noteRemoteDataSource.createNote(noteEntity: noteEntity);
       return right(null);
+    } on DioException catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
     } catch (e) {
-      if (e is DioException) {
-        return left(ServerFailure.fromDioError(e));
-      } else {
-        return left(ServerFailure(errMessage: e.toString()));
-      }
+      return left(ServerFailure(errMessage: "[LOCAL-DB]:Error writing notes!"));
     }
   }
 
@@ -51,34 +53,36 @@ class NoteRepositoryImpl extends NoteRepository {
     required NoteEntity updatedNoteEntity,
   }) async {
     try {
-      await noteLocalDataSource.updateNote(
-        updatedNoteEntity: updatedNoteEntity,
-      );
+      // await noteLocalDataSource.updateNote(
+      //   updatedNoteEntity: updatedNoteEntity,
+      // );
       await noteRemoteDataSource.updateNote(
         updatedNoteEntity: updatedNoteEntity,
       );
+
       return right(null);
+    } on DioException catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
     } catch (e) {
-      if (e is DioException) {
-        return left(ServerFailure.fromDioError(e));
-      } else {
-        return left(ServerFailure(errMessage: e.toString()));
-      }
+      return left(
+        ServerFailure(errMessage: "[LOCAL-DB]:Error updating notes!"),
+      );
     }
   }
 
   @override
   Future<Either<Failure, void>> deleteNote({required String noteId}) async {
     try {
-      await noteLocalDataSource.deleteNote(noteId: noteId);
+      // await noteLocalDataSource.deleteNote(noteId: noteId);
+      // Just for testing
       await noteRemoteDataSource.deleteNote(noteId: noteId);
       return right(null);
+    } on DioException catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
     } catch (e) {
-      if (e is DioException) {
-        return left(ServerFailure.fromDioError(e));
-      } else {
-        return left(ServerFailure(errMessage: e.toString()));
-      }
+      return left(
+        ServerFailure(errMessage: "[LOCAL-DB]:Error deleting notes!"),
+      );
     }
   }
 }
